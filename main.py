4d30f3caca83e269356bba7f12ba5c701a310660
Niels ten Boom <nielstenboom@gmail.com>
2023-02-25 14:24:02 +0100

    try compare url

--- main.py
+++ main.py
@@ -63,6 +63,13 @@ def main(base: str, head: str):
         output_lines.extend(chore)
 
 
+    project_remote = run_command(f"git config --local remote.origin.url")
+    project_name = project_remote.split(":")[-1].replace(".git", "")
+    compare_url = f"https://github.com/{project_name}/compare/{base}...{head}"
+
+    # add compare url to output as markdown url
+    output_lines.append(f"\n\n\n[{base}...{head}]({compare_url})")
+
     output = "\n".join(output_lines)
 
     # use '<br>' as delimiter for gh actions output
